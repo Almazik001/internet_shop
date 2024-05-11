6e3b7eff896630ba27269e208190b3dd3a8d5c35
Almazik001 <almazshasaitov169@gmail.com>
2024-05-11 23:09:23 +0500

    second

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+const apiError = require('../error/apiError')
 class userController {
     async registration(req, res) {
         
@@ -7,8 +8,11 @@ class userController {
         
     }
 
-    async check(req, res) {
+    async check(req, res, next) {
         const {id} = req.query
+        if (!id) {
+            return next(apiError.badRequest('Не задан ID'))
+        }
         res.json(id)
     }
 }
--- server/index.js
+++ server/index.js
@@ -4,14 +4,18 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const router = require('./routes/index')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const errorHandler = require('./middleware/ErrorHandlingMiddleWare')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
-
+// Обработка ошибок, последний MiddleWare
+app.use(errorHandler)
 
 const start = async () => {
     try {
